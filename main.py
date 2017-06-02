0014c646fcdf4440ba63104bb32f2f20e786eccc
Sun Xiaofei <adoni1203@gmail.com>
2017-06-02 16:56:05 +0800

    simplify the code

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from graph import Graph
 from model import Net
+from torch.autograd import Variable
+import torch
+import torch.nn as nn
 import torch.optim as optim
 from tqdm import tqdm
 import sys
-
 
 def main():
     graph_pos = sys.argv[1]
@@ -12,15 +14,16 @@ def main():
     emb_dimension = 100
     net = Net(emb_size, emb_dimension)
     optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
-    for i in tqdm(range(10000)):
+    for i in tqdm(range(3)):
         running_loss = 0.0
         for edge in tqdm(graph.edge_list):
             neg_edges = graph.negative_sampling(edge, 20)
             optimizer.zero_grad()
-            loss = net.forward(edge, neg_edges)
+            loss = net(edge, neg_edges)
             loss.backward()
             optimizer.step()
             running_loss += loss.data[0]
+        #print(running_loss)
         running_loss=0.0
 
 
--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import torch
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import CosineEmbeddingLoss
 
 
 class Net(nn.Module):
@@ -16,36 +15,32 @@ class Net(nn.Module):
         self.emb_dimension = emb_dimension
         self.u_embeddings = nn.Embedding(emb_size, emb_dimension, sparse=True)
         self.v_embeddings = nn.Embedding(emb_size, emb_dimension, sparse=True)
+        self.criterion = nn.CosineEmbeddingLoss()
         self.init_emb()
 
     def init_emb(self):
         initrange = 0.5 / self.emb_dimension
         self.u_embeddings.weight.data.uniform_(-initrange, initrange)
         self.v_embeddings.weight.data.uniform_(-0, 0)
-        self.loss_function=CosineEmbeddingLoss(margin=1.0)
 
     def forward(self, edge, negative_edges):
         emb_u = self.u_embeddings(Variable(torch.LongTensor([edge.u])))
         emb_v = self.v_embeddings(Variable(torch.LongTensor([edge.v])))
+        losses=[]
+        #target = Variable(torch.LongTensor([1]))
+        #losses.append(self.criterion(emb_u, emb_v, target))
         score = torch.dot(emb_u, emb_v)
         score = F.logsigmoid(score)
-        #score=self.loss_function(emb_u, emb_v, Variable(torch.LongTensor([1])))
-        scores = [score]
-        assert len(negative_edges)<=20
+        losses.append(-1*score)
+        return losses[0]
         for edge in negative_edges:
-            emb_u = self.u_embeddings(Variable(torch.LongTensor([edge.u])))
-            emb_v = self.v_embeddings(Variable(torch.LongTensor([edge.v])))
-            #score=self.loss_function(emb_u, emb_v, Variable(torch.LongTensor([-1])))
-            score = torch.dot(emb_u, emb_v)
-            score = F.logsigmoid(-1 * score)
-            scores.append(score)
-        # print(scores)
-        loss = -1 * sum(scores)
-        return loss
+            neg_emb_u = self.u_embeddings(Variable(torch.LongTensor([edge.u])))
+            neg_emb_v = self.v_embeddings(Variable(torch.LongTensor([edge.v])))
+            neg_target = Variable(torch.LongTensor([-1]))
+            losses.append(self.criterion(neg_emb_u, neg_emb_v, neg_target))
+        #     scores.append(score)
+        # # print(scores)
+        # loss = -1 * sum(scores)
+        #return sum(losses)
+        return losses[0]
 
-    # def save_embedding(self, to_be_saved_node_name, graph):
-    #     for node_name in to_be_saved_node_name:
-    #         nid = graph.name_to_id[node_name]
-    #         nid = Variable(torch.LongTensor([nid]))
-    #         emb = self.embeddings(nid).data[0]
-    #         print(emb)
